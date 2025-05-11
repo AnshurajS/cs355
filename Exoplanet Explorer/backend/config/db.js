e7588db9ff283b10527e4c8802ae20b3aaad59e1
Anshuraj Sedai <124818768+AnshurajS@users.noreply.github.com>
2025-05-10 21:38:23 -0400

    Delete Exoplanet Explorer directory

--- Exoplanet Explorer/backend/config/db.js
+++
@@ -1,6 +0,0 @@
-const Datastore = require('nedb-promises');
-
-// Single users database
-const usersDB = Datastore.create({ filename: 'data/users.db', autoload: true });
-
-module.exports = { usersDB };
--- Exoplanet Explorer/backend/models/User.js
+++
@@ -1,8 +0,0 @@
-const { usersDB } = require('../config/db');
-
-module.exports = {
-  findByEmail: email => usersDB.findOne({ email }),
-  findById: id => usersDB.findOne({ _id: id }),
-  create: user => usersDB.insert(user),
-  updateFavorites: (id, favs) => usersDB.update({ _id: id }, { $set: { favorites: favs } })
-};
